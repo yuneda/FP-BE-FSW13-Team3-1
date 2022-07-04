8a0586c6124995dd60e70a8a4d09c7eb03f711b5
Nur Aini Lailla Asri <laillaasri@gmail.com>
2022-07-04 20:19:07 +0700

    [Update] jest testing register

--- __test__/v1/Auth/login.test.js
+++ __test__/v1/Auth/login.test.js
@@ -1,37 +1,73 @@
 const request = require('supertest');
 const bcrypt = require('bcryptjs');
 const app = require('../../../app');
-// const { User } = require('../../../app/models');
 
 describe('Login', () => {
   const emailLogin = 'admin@mail.com';
-  // const emailNotRegistered = 'custnotregis@mail.com';
+  const emailNotRegistered = 'custnotregis@mail.com';
   const passwordLogin = '123456';
-  // const passwordNotRegistered = 'custnotregis';
-  const passwordHash = bcrypt.hashSync(passwordLogin, 10);
-
-  // beforeEach(async () => {
-  //   await User.create(customer);
-  //   const user = await User.findOne({ where: { email: emailNotRegistered } });
-
-  //   if (user != null) {
-  //     await user.destroy({ where: { email: emailNotRegistered } });
-  //   }
-  // });
-
-  // afterEach(async () => {
-  //   const user = await User.findOne({ where: { email: emailLogin } });
-  //   await user.destroy({ where: { email: emailLogin } });
-  // });
+  const passwordNotRegistered = 'custnotregis';
 
   it('Login success, status code 201', async () => request(app)
     .post('/api/v1/login')
     .set('Content-Type', 'application/json')
     .send({ email: emailLogin, password: passwordLogin })
     .then((res) => {
-      console.log(res)
-      expect(res.statusCode).toBe(201);
+      expect(res.statusCode).toBe(200);
       expect(res.body.accesToken).toEqual(res.body.accesToken);
-    })
-  )
+    }))
+
+  it('Login status code 404', async () => request(app)
+  .post('/api/v1/login')
+  .set('Content-Type', 'application/json')
+  .send({ email: emailNotRegistered, password: passwordNotRegistered })
+  .then((res) => {
+    expect(res.statusCode).toBe(404);
+    expect(res.body).toEqual({
+      error: {
+        message: 'Email Not Found!'
+      },
+    });
+  }))
+
+  it('Login status code 401', async () => request(app)
+  .post('/api/v1/login')
+  .set('Content-Type', 'application/json')
+  .send({ email: emailLogin, password: passwordNotRegistered })
+  .then((res) => {
+    expect(res.statusCode).toBe(401);
+    expect(res.body).toEqual({
+      error: {
+        message: 'Wrong Password. Please Try Again!'
+      },
+    });
+  }))
+
+  it('Login email not found status code 404', async () => request(app)
+  .post('/api/v1/login')
+  .set('Content-Type', 'application/json')
+  .send({ email: emailNotRegistered, password: passwordLogin })
+  .then((res) => {
+    expect(res.statusCode).toBe(404);
+    expect(res.body).toEqual({
+      error: {
+        message: 'Email Not Found!'
+      },
+    });
+  }))
+
+  // it('Not login status code 401', async () => request(app)
+  // .post('/api/v1/login')
+  // .set('Content-Type', 'application/json')
+  // // .send({})
+  // .then((res) => {
+  //   console.log(res.statusCode)
+  //   console.log(res.body)
+  //   // expect(res.statusCode).toBe(404);
+  //   // expect(res.body).toEqual({
+  //   //   error: {
+  //   //     message: 'Please login first'
+  //   //   },
+  //   // });
+  // }))
 })
---
+++ __test__/v1/Auth/register.js
@@ -0,0 +1,81 @@
+const { INTEGER } = require('sequelize');
+const request = require('supertest');
+// const bcrypt = require('bcryptjs');
+const app = require('../../../app');
+const { User } = require('../../../app/models');
+
+describe('Register', () => {
+  afterAll(async () => {
+    await User.destroy({ where: { email: 'user@gmail.com' } });
+  });
+
+  it('Register with status code 201', () => request(app)
+    .post('/api/v1/register')
+    .set('Accept', 'application/json')
+    .send({
+      name: 'user',
+      email: 'user@gmail.com',
+      password: '123456',
+    })
+    .then((res) => {
+      expect(res.statusCode).toBe(201);
+      expect(res.body).toEqual({
+        status: expect.any(String),
+        message: expect.any(String),
+        data: expect.any(Object),
+      })
+    }))
+
+  it('Register with exciting mail status code 422', () => request(app)
+    .post('/api/v1/register')
+    .set('Accept', 'application/json')
+    .send({
+      name: 'user',
+      email: 'user@gmail.com',
+      password: '123456',
+    })
+    .then((res) => {
+      // console.log(res.statusCode)
+      // console.log(res.body)
+      expect(res.statusCode).toBe(422);
+      expect(res.body).toEqual({
+        error: {
+          message: expect.any(String)
+        }
+      })
+    }))
+
+    it('Register with lengh password error status code 400', () => request(app)
+    .post('/api/v1/register')
+    .set('Accept', 'application/json')
+    .send({
+      name: 'user',
+      email: 'user@gmail.com',
+      password: '123',
+    })
+    .then((res) => {
+      expect(res.statusCode).toBe(400);
+      expect(res.body).toEqual({
+        error: {
+          message: expect.any(String)
+        }
+      })
+    }))
+
+    it('Register with email format error status code 400', () => request(app)
+    .post('/api/v1/register')
+    .set('Accept', 'application/json')
+    .send({
+      name: 'user',
+      email: 'usergmail.com',
+      password: '123456',
+    })
+    .then((res) => {
+      expect(res.statusCode).toBe(400);
+      expect(res.body).toEqual({
+        error: {
+          message: expect.any(String)
+        }
+      })
+    }))
+})

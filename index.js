5a85d141e5f8153d69036506517345ec26856faa
yuneda <alfajryuneda@gmail.com>
2022-12-18 18:02:15 +0700

    change start

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 
-const app = express();
-const port = process.env.PORT;
+// const app = express();
+const app = require('./app');
+const port = 8000;
 
 app.get('/', (req, res) => {
   res.send('YUNEDA TEAM !');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Listening on http://localhost:${port}`);
 });
